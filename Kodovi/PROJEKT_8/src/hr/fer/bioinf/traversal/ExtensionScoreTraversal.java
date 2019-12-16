988c5c5f8bf068bde1c22be9546536aa5845e1b6
fabijancorak <fc49106@fer.hr>
2019-12-16 23:39:11 +0100

    Remove unused imports.

--- Kodovi/PROJEKT_8/src/hr/fer/bioinf/traversal/ExtensionScoreTraversal.java
+++ Kodovi/PROJEKT_8/src/hr/fer/bioinf/traversal/ExtensionScoreTraversal.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import hr.fer.bioinf.graph.Edge;
 import hr.fer.bioinf.graph.Graph;
