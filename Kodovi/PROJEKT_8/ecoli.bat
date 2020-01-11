5ba952fc2d5ec0eb9c3507f7d6bfc2e8131a0b69
fabijancorak <fc49106@fer.hr>
2020-01-11 20:22:10 +0100

    Make a functional ecoli batch script.

---
+++ Kodovi/PROJEKT_8/ecoli.bat
@@ -0,0 +1,7 @@
+java -cp bin hr.fer.bioinf.Main^
+  --sequence-identity-cutoff=0.6^
+  --monte-carlo-iterations=50000^
+  --contigs-path="../../../dataset/EColi - synthetic\ecoli_test_contigs.fasta"^
+  --reads-path="../../../dataset/EColi - synthetic\ecoli_test_reads.fasta"^
+  --contigs-reads-overlaps-path="data/reads_contigs_overlaps.paf"^
+  --reads-overlaps-path="data/reads_reads_overlaps.paf"
