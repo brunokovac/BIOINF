57893671433ad5fbc3fe79bbeb37b02ce5fa1417
fabijancorak <fc49106@fer.hr>
2020-01-11 19:29:33 +0100

    Add bat script for running. Modify classpath.

---
+++ Kodovi/PROJEKT_8/cjejuni.bat
@@ -0,0 +1,7 @@
+java -cp bin hr.fer.bioinf.Main^
+  --sequence-identity-cutoff=0.6^
+  --monte-carlo-iterations=50000^
+  --contigs-path="../../../dataset/CJejuni - real/cjejuni_contigs.fasta"^
+  --reads-path="../../../dataset/CJejuni - real/cjejuni_reads.fasta"^
+  --contigs-reads-overlaps-path="../../../dataset/CJejuni - real/cjejuni_contig_overlaps.paf"^
+  --reads-overlaps-path="../../../dataset/CJejuni - real/cjejuni_read_overlaps.paf"
